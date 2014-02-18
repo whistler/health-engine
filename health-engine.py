4c3db5f6356967ca646380cf87b038cfc0c2dfc5
Huang <judezhu@repun.(none)>
2014-02-17 21:22:12 -0800

    add a post rest API and modified .gitignore.

--- health-engine.py
+++ health-engine.py
@@ -12,6 +12,17 @@ def recommend():
     print request
     response = {
         'id': 0,
-        'recommendation': 'Hello World!'
+        'recommendation': 'Hello, Welcome to our search engine!'
     }
     return json.dumps(response)
+
+@app.route('/', methods = ['POST'])
+def recommend_post():
+    if not request.json or not 'steps' in request.json:
+       abort(400)
+    response = {
+        'id': 0,
+        'recommendation': 'You are in a good situation'
+    }
+    return json.dumps(response)
+
