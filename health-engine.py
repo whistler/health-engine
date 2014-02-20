d290d001b739e657dc4041552a02380d5fe05d67
Huang <judezhu@repun.(none)>
2014-02-20 12:27:12 -0800

    add sleep recommendation.

--- health-engine.py
+++ health-engine.py
@@ -6,6 +6,8 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
+recommendations = {203:'Prehypertention Symptoms found! Please check your BP again after 6 hrs.', 
+                   318:'Sleep at least 8 hrs, take rest, your bones and joints needs extra care that have arthiritis problem'}
 
 @app.route('/')
 def recommend():
@@ -18,11 +20,15 @@ def recommend():
 
 @app.route('/', methods = ['POST'])
 def recommend_post():
-    if not request.json or not 'steps' in request.json:
-       abort(400)
-    response = {
-        'id': 0,
-        'recommendation': 'You are in a good situation'
-    }
-    return json.dumps(response)
+    response = [];
+    
+    if not request.json or not 'sleep' in request.json:
+       abort(400);
+       
+    if int(request.json['sleep']) < 8:
+       response.append({'id':318, 'recommendation': recommendations.get(318)})
+
+    else:
+        response.append({'id':0, 'recommendation':'Congratulations,You have a good sleep!'})
+    return json.dumps(response[0])
 
