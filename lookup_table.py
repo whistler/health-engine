6feb6d2c3a363e9c72f7cf874600b6f0af267df7
Huang <judezhu@repun.(none)>
2014-02-27 17:46:29 -0800

    remove gspread

--- lookup_table.py
+++ lookup_table.py
@@ -22,15 +22,6 @@ def lookup(features):
 
 def load_recommendations():
     """ Reads the recommendations table from disk and returns it """
-    import gspread
-    # Login with your Google account
-    gc = gspread.login('account', 'password')
-    
-    # Open a worksheet from spreadsheet with one shot
-    wks = gc.open("Medical Research").sheet1
-    
-    # Get all values from the first row
-    # With label
-    val = wks.acell('B1').value
-    
-    return val
+    import csv
+
+    pass
