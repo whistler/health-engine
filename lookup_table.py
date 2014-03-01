aa7982909018a52c58074bd00f18d2148568cb18
Huang <judezhu@repun.(none)>
2014-02-28 21:55:09 -0800

    look_up table changed.

--- lookup_table.py
+++ lookup_table.py
@@ -5,36 +5,45 @@ table = None # Stores all the recommendations
 
 from features import Features
 
-def lookup(features):
+#def lookup(features):
+def lookup():
     
     recommendations = []
     
-    #Some data of feature, For testing
-    feature=[]
-    feature[0]=Features(100, 120, 60, 80, None, None, None, None, 0, 105, 65, 200)
-    feature[1]=Features(120, 139, 80, 89, None, None, None, None, 0, 105, 19, 65)
-    feature[2]=Features(140, 159, 90, 99, None, None, None, None, None, None, None, None)
-   
-
     # check if table is already loaded
     if 'table' in vars() or 'table' in globals():
         table = load_recommendations()
-        
+        for k in table.keys():
+            print table[k]
+     
+    return recommendations    
     #TODO: Loop over the recommendations table and append the ones the meet the criteria in features
-    # to recommendations list
-    
+    # to recommendations list   
+    '''    
     stub = {'feature0': feature[0], 'recommendation0': "Test recommendation" }
     stub = {'feature1': feature[1], 'recommendation1': "Test recommendation" }
     stub = {'feature2': feature[2], 'recommendation2': "Test recommendation" }
 
     recommendations.append(stub)
-    
-    return recommendations
-    
+    '''
+  
+
 
 def load_recommendations():
+    table_temp = {};
     """ Reads the recommendations table from disk and returns it """
     import csv
-
-    pass
+    
+    #Todo: add try block
+    with open('recom.csv', 'rb') as f:
+        reader = csv.reader(f)
+        #Todo: Check how many roles there 
+        for row in reader:
+          #  print row
+            key = Features(row[1], row[2], row[3], row[4],
+                 row[5], row[6], row[7], row[8], row[9], row[10],
+                 row[11], row[12])
+            table_temp[key] = row[13];
+        #return "hello"
+        return table_temp;
 
