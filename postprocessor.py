155e99d579dac24d7891d6509ee2b5263dd75053
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2014-03-30 17:08:41 -0700

    add comment to postprocessor

--- postprocessor.py
+++ postprocessor.py
@@ -1,2 +1,5 @@
+""" Combines the recommendations produced by the two recommenders and suggests 
+if a current problem might be caused by earlier problems. """
+
 def process(instance_recommendations, timeseries_recommendations):
     return None
