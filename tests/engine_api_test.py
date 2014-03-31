3f8b6a6c28f55828a57404543cff635b26680270
Huang <judezhu@repun.(none)>
2014-03-31 11:42:51 -0700

    add api testing framework.

---
+++ tests/engine_api_test.py
@@ -0,0 +1,47 @@
+'''
+Created on Mar 27, 2014
+
+@author: judezhu
+'''
+import unittest
+import recommender
+from generate_input_json import get_input_json
+
+class Test(unittest.TestCase):
+
+# Todo: Blood Pressure Test Suite
+
+    def setUp(self):
+        self.input_json = get_input_json()
+    
+    #200: Normal Blood Pressure
+    def testBP200(self):
+ #       inputs = json.load(Fixture_Path + "110_Normal_Blood_Pressure.json")
+ #       self.assertEqual(200, recommender.recommend(inputs).getId(), "testing bp 200")
+        pass
+        
+    #210: Abnormal Blood Pressure
+    def testBP210(self):
+        pass
+    
+    #240: Abnormal Blood Pressure
+    def testBP220(self):
+        pass
+
+# Todo: Heart Beats Test Suite
+        
+    #310: Normal Blood Pressure
+    def testHB310(self):
+        pass
+        
+    #310: Normal Blood Pressure
+    def testActivity110(self):
+        pass
+
+    #310: Normal Blood Pressure
+    def testActivity111(self):
+        pass
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
