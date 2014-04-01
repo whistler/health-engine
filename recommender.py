0c8031a0d47b879bf83f33afde36138454d4b56a
haijunq <haijunq@gmail.com>
2014-03-31 17:26:36 -0700

    time series testing

--- recommender.py
+++ recommender.py
@@ -2,6 +2,11 @@
 described in the API and returns a python object with recommendations in the 
 format described by the API. """
 
+"""
+temporily comment the instance_recommendations out for testing
+
+"""
+
 # import instance_recommendations
 import analyze_timeseries
 
